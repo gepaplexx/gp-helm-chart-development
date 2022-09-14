aa07ac8daf2cde11e9f05f54d5923466880f375b
smahmody <shahin.mahmody@gepardec.com>
2022-09-14 14:05:45 +0200

    feature/GPX-416 help, usage, flag for non-repeatable actions, use of previously generated token

--- infra/gp-hashicorp-vault/helpers/vault-setup.sh
+++ infra/gp-hashicorp-vault/helpers/vault-setup.sh
@@ -10,42 +10,91 @@
 #TODO:
 #  * CLEANUP
 #  * Define Default Roles and Capabilities. Currently it's just a start and PoC
-#  * additional parameter to not execute commands that cannot be executed multiple times.
-#      e.g. auth method enablement
-#  * write usage message and print in case of failure
+
+PRG=$(basename $0)
+DIR=$(dirname $0)
+
+namespace="gp-vault"
+skip_non_repeatable=false
+
+usage() {
+  echo "Usage: $PRG [-h|s] cluster_name client_secret [access_token]"
+}
+
+help() {
+  echo "Sets up the base HashiCorp Vault installation"
+  echo
+  usage
+  echo "options:"
+  echo "h                 displays help"
+  echo "s                 skip non-repeatable actions. Requires access_token parameter to be set. Warning: this will override changes in some policies and configs! "
+  echo
+  echo "parameters:"
+  echo "cluster_name      cluster's name, used in oidc redirect url setup"
+  echo "client_secret     client secret for oidc setup"
+  echo "access_token      OPTIONAL: a sufficiently privileged Vault token to perform the setup with (skips initialization). Required with -s"
+}
+
+while getopts ":hs" option; do
+   case $option in
+      h)
+        help
+        exit;;
+      s)
+        skip_non_repeatable=true
+        ;;
+      \?)
+        usage
+        exit;;
+   esac
+done
+
+if [ "${skip_non_repeatable}" = true ]; then
+  shift
+fi
 
 
 CLUSTER="${1}"
 CLIENT_SECRET="${2}"
+ACCESS_TOKEN="${3}"
 
 if [ -z "${CLIENT_SECRET}" ]; then
-  echo "client secret not set. Exiting...."
+  usage
   exit 1
 fi
 if [ -z "${CLUSTER}" ]; then
-  echo "Cluster not set. Exiting...."
+  usage
+  exit 1
+fi
+if [[ -z "${ACCESS_TOKEN}" ]] && [[ "${skip_non_repeatable}" = true ]]; then
+  echo "Skipping non repeatable actions requires an access token!"
+  echo
+  usage
   exit 1
 fi
 
-PRG=`basename $0`
-DIR=$(dirname $0)
-
 set -e
 
-namespace="gp-vault"
+echo "Starting Vault setup..."
+echo "==============================================="
+
 
 # INITIALIZE VAULT
-echo "initializing vault & waiting 10 seconds for initialization to be complete. don't worry. be happy. chill out. and grab a cigarette"
-kubectl exec vault-0 -n "${namespace}" -- sh -c "vault operator init -recovery-shares=3 -recovery-threshold=3 -format=json"  > "${DIR}"/vault-init.json
-sleep 10  # Required to ensure vault is correctly initialized and unsealed from GCP.
-ROOT_TOKEN=$(cat "${DIR}"/vault-init.json | jq -r .root_token)
+if [ -z "${ACCESS_TOKEN}" ]; then
+  echo "initializing vault & waiting 10 seconds for initialization to be complete. don't worry. be happy. chill out. and grab a cigarette"
+  kubectl exec vault-0 -n "${namespace}" -- sh -c "vault operator init -recovery-shares=3 -recovery-threshold=3 -format=json"  > "${DIR}"/vault-init.json
+  sleep 10  # Required to ensure vault is correctly initialized and unsealed from GCP.
+  ACCESS_TOKEN=$(cat "${DIR}"/vault-init.json | jq -r .root_token)
+fi
 
 # DEFAULT SECRET STORE FOR CICD
-kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ROOT_TOKEN}  && vault secrets enable -path=development/cicd kv-v2"
+if [ "${skip_non_repeatable}" = false ]; then
+  kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ACCESS_TOKEN}  && vault secrets enable -path=development/cicd kv-v2"
+fi
 
 # DEFAULT POLICIES
 # CICD-READER
-kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ROOT_TOKEN}  && \
+kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ACCESS_TOKEN}  && \
     vault policy write cicd-reader \
       <(echo 'path \"/development/cicd/*\"
         {
@@ -53,7 +102,7 @@ kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ROOT_TO
         }'
       )"
 
-kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ROOT_TOKEN}  && \
+kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ACCESS_TOKEN}  && \
     vault policy write cicd-admin \
       <(echo 'path \"/development/cicd/*\"
         {
@@ -62,7 +111,7 @@ kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ROOT_TO
       )"
 
 # ADMIN
-kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ROOT_TOKEN} && \
+kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ACCESS_TOKEN} && \
     vault policy write admin \
       <(echo '
         # Read system health check
@@ -138,9 +187,11 @@ kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ROOT_TO
       )"
 
 # ENABLE & CONFIGURE KUBERNETES AUTH INTEGRATION
-kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ROOT_TOKEN} && vault auth enable kubernetes"
-kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ROOT_TOKEN} && vault write auth/kubernetes/config kubernetes_host=https://\${KUBERNETES_SERVICE_HOST}:\${KUBERNETES_SERVICE_PORT}"
-kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ROOT_TOKEN} && \
+if [ "${skip_non_repeatable}" = false ]; then
+  kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ACCESS_TOKEN} && vault auth enable kubernetes"
+fi
+kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ACCESS_TOKEN} && vault write auth/kubernetes/config kubernetes_host=https://\${KUBERNETES_SERVICE_HOST}:\${KUBERNETES_SERVICE_PORT}"
+kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ACCESS_TOKEN} && \
     vault write auth/kubernetes/role/cicd-reader \
       bound_service_account_names=operate-workflow-sa \
       bound_service_account_namespaces='*' \
@@ -148,9 +199,11 @@ kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ROOT_TO
       ttl=24h"
 
 # ENABLE & CONFIGURE OIDC AUTH INTEGRATION
-kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ROOT_TOKEN} && vault auth enable oidc"
+if [ "${skip_non_repeatable}" = false ]; then
+  kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ACCESS_TOKEN} && vault auth enable oidc"
+fi
 # DEFAULT ROLE
-kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ROOT_TOKEN} && \
+kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ACCESS_TOKEN} && \
     vault write auth/oidc/role/default \
        bound_audiences='vault' \
        allowed_redirect_uris='https://vault-ui-gp-vault.apps.${CLUSTER}.gepaplexx.com/ui/vault/auth/oidc/oidc/callback' \
@@ -159,7 +212,7 @@ kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ROOT_TO
        policies='default' \
        groups_claim='groups'"
 
-kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ROOT_TOKEN} && \
+kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ACCESS_TOKEN} && \
     vault write auth/oidc/config \
       oidc_client_id='vault' \
       oidc_discovery_url='https://sso.apps.${CLUSTER}.gepaplexx.com/realms/internal' \
@@ -167,38 +220,46 @@ kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ROOT_TO
       default_role=default"
 
 # GROUP MAPPING
+# Standard output gets piped to /dev/null for more uniform responses
 
-OIDC_AUTH_ACCESSOR=$(kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ROOT_TOKEN} && \
+OIDC_AUTH_ACCESSOR=$(kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ACCESS_TOKEN} && \
       vault auth list -format=json" | jq -r '."oidc/".accessor')
 
-kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ROOT_TOKEN} && \
+kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ACCESS_TOKEN} && \
     vault write identity/group \
       name='Gepaplexx' \
       type='external' \
       policies='admin,cicd-admin' \
-      metadata=responsibility='Vault Admin'"
+      metadata=responsibility='Vault Admin'" 1> /dev/null
+echo "Success! Data written to: identity/group/name/Gepaplexx"
 
-GROUP_ID=$(kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ROOT_TOKEN} && \
-      vault read -field=id identity/group/name/Gepaplexx")
-kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ROOT_TOKEN} && \
-      vault write identity/group-alias name='Gepaplexx' \
-         mount_accessor=${OIDC_AUTH_ACCESSOR} \
-         canonical_id=${GROUP_ID}"
+if [ "${skip_non_repeatable}" = false ]; then
+  GROUP_ID=$(kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ACCESS_TOKEN} && \
+        vault read -field=id identity/group/name/Gepaplexx")
+  kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ACCESS_TOKEN} && \
+        vault write identity/group-alias name='Gepaplexx' \
+           mount_accessor=${OIDC_AUTH_ACCESSOR} \
+           canonical_id=${GROUP_ID}" 1> /dev/null
+  echo "Success! Data written to: identity/group-alias/"
+fi
 
-
-kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ROOT_TOKEN} && \
+kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ACCESS_TOKEN} && \
     vault write identity/group \
       name='Gepardec' \
       type='external' \
       policies='cicd-admin' \
-      metadata=responsibility='Vault CICD Admin'"
+      metadata=responsibility='Vault CICD Admin'" 1> /dev/null
+echo "Success! Data written to: identity/group/name/Gepardec"
 
-GROUP_ID=$(kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ROOT_TOKEN} && \
-      vault read -field=id identity/group/name/Gepardec")
-kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ROOT_TOKEN} && \
-      vault write identity/group-alias name='Gepardec' \
-         mount_accessor=${OIDC_AUTH_ACCESSOR} \
-         canonical_id=${GROUP_ID}"
+if [ "${skip_non_repeatable}" = false ]; then
+  GROUP_ID=$(kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ACCESS_TOKEN} && \
+        vault read -field=id identity/group/name/Gepardec")
+  kubectl exec vault-0 -n "${namespace}" -- sh -c "vault login -no-print ${ACCESS_TOKEN} && \
+        vault write identity/group-alias name='Gepardec' \
+           mount_accessor=${OIDC_AUTH_ACCESSOR} \
+           canonical_id=${GROUP_ID}" 1> /dev/null
+  echo "Success! Data written to: identity/group-alias/"
+fi
 
 echo "==============================================="
 echo "donesies. better have that cigarette finished."
