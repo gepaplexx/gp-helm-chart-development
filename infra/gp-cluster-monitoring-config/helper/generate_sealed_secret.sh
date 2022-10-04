6eb3c27926a760ba364b83b6f49f44457505ef74
fhochleitner <felix.hochleitner@outlook.com>
2022-10-04 08:47:39 +0200

    GPX-489 Certificate sealed-secret expired
    use autorenewed certificate/key for en-/decryption
    
    Signed-off-by: fhochleitner <felix.hochleitner@outlook.com>

--- infra/gp-cluster-monitoring-config/helper/generate_sealed_secret.sh
+++ infra/gp-cluster-monitoring-config/helper/generate_sealed_secret.sh
@@ -9,5 +9,6 @@ then
 fi
 
 echo -e "Dein neues Sealed Secret, füge es ein in cluster-applications/gp-cluster-applications/values/values-*.yaml -> clusterMonitoring.parameters.alertmanager.config:\n"
-oc create secret generic alertmanager-main --from-file=alertmanager.yaml=${filename} --dry-run=client -o yaml -n openshift-monitoring | kubeseal -o yaml --cert <(oc -n gp-infrastructure get secret sealed-secret-keys -o jsonpath="{.data.tls\.crt}" | base64 -d) | grep alertmanager.yaml | tr -s " " | cut -d " " -f3
+CURRSECRET=$(kubectl -n gp-infrastructure get secret --sort-by metadata.creationTimestamp | grep sealed-secrets-key | head -n 1 | cut -d " " -f 1)
+oc create secret generic alertmanager-main --from-file=alertmanager.yaml=${filename} --dry-run=client -o yaml -n openshift-monitoring | kubeseal -o yaml --cert <(oc -n gp-infrastructure get secret "${CURRSECRET}" -o jsonpath="{.data.tls\.crt}" | base64 -d) | grep alertmanager.yaml | tr -s " " | cut -d " " -f3
 echo -e "\nACHTUNG: Du verwendest gerade: $(oc whoami --show-server=true)\n"
--- infra/gp-identity-provider/helper/generate_encrypted_value.sh
+++ infra/gp-identity-provider/helper/generate_encrypted_value.sh
@@ -11,5 +11,6 @@ then
 fi
 
 echo -e "The encrypted Value: \n"
-oc create secret generic ${secretname} --from-file=value=${filename} --dry-run=client -o yaml -n ${namespace} | kubeseal -o yaml --cert <(oc -n gp-infrastructure get secret sealed-secret-keys -o jsonpath="{.data.tls\.crt}" | base64 -d)
+CURRSECRET=$(kubectl -n gp-infrastructure get secret --sort-by metadata.creationTimestamp | grep sealed-secrets-key | head -n 1 | cut -d " " -f 1)
+oc create secret generic ${secretname} --from-file=value=${filename} --dry-run=client -o yaml -n ${namespace} | kubeseal -o yaml --cert <(oc -n gp-infrastructure get secret "${CURRSECRET}" -o jsonpath="{.data.tls\.crt}" | base64 -d)
 echo -e "\nACHTUNG: Du verwendest gerade: $(oc whoami --show-server=true)\n"
