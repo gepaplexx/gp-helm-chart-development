06ac9cf51e7a9d85d04b992e4b80d0611670c05c
lucostus <constantin.winkler@gepardec.com>
2022-10-21 14:52:26 +0200

    GPX-471: Updated to test all certs

--- infra/gp-cluster-monitoring-config/helper/decrypt_alertmanager_config_from_values.sh
+++ infra/gp-cluster-monitoring-config/helper/decrypt_alertmanager_config_from_values.sh
@@ -2,8 +2,7 @@
 
 valuesFileSuffix=play
 
-if [ -n "$1" ];
-then
+if [ -n "$1" ]; then
   valuesFileSuffix=$1
 fi
 
@@ -11,5 +10,17 @@ valuesFile="../../../../gepardec-run-cluster-configuration/cluster-applications/
 
 echo -e "\nACHTUNG: Du verwendest gerade: $(oc whoami --show-server=true)\nUnd folgendes values File: ${valuesFile}"
 
-CURRENTSECRET=$(kubectl -n gp-infrastructure get secret --sort-by metadata.creationTimestamp | grep sealed-secrets-key | tail -n 1 | cut -d " " -f 1)
-kubeseal --recovery-private-key <(oc -n gp-infrastructure get secret ${CURRENTSECRET} -o jsonpath="{.data.tls\.key}" | base64 -d) --recovery-unseal -f <(helm template -s templates/08-alertmanager-config-sealed-secret.yaml --set alertmanager.config=$(grep -A2 alertmanager.config  ${valuesFile} | grep value | cut -d '"' -f2) ..) -o yaml | grep -o "alertmanager.yaml.*" | cut -d " " -f2 | base64 -d | tee alertmanager-from-values-${valuesFileSuffix}.yml
+CERTS=$(kubectl -n gp-infrastructure get secret --sort-by metadata.creationTimestamp | grep sealed-secrets-key | wc -l)
+
+for CERT in $(kubectl -n gp-infrastructure get secret --sort-by metadata.creationTimestamp | egrep "sealed-secret.*-key" | grep kubernetes | cut -d " " -f1); do
+  COMMAND=$(kubeseal --recovery-private-key <(oc -n gp-infrastructure get secret ${CERT} -o jsonpath="{.data.tls\.key}" | base64 -d) --recovery-unseal -f <(helm template -s templates/08-alertmanager-config-sealed-secret.yaml --set alertmanager.config=$(grep -A2 alertmanager.config ${valuesFile} | grep value | cut -d '"' -f2) ..) -o yaml | grep -o "alertmanager.yaml.*" | cut -d " " -f2 | base64 -d | tee alertmanager-from-values-${valuesFileSuffix}.yml)
+
+  if COMMAND; then
+    exit 0
+  else
+    echo '>>> next cert '
+  fi
+done
+
+echo "Kein Zertifikat hat funktioniert :c"
+
