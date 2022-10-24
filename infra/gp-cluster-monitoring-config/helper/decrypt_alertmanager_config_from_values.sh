f90a8dc2b2e1ea154d32ec61d518474422458c29
Herbert Wirnsberger <herbert.wirnsberger@gepardec.com>
2022-10-24 13:49:07 +0200

    GPX-471 - Use label to select sealed secret kubernetes secrets

--- infra/gp-cluster-monitoring-config/helper/decrypt_alertmanager_config_from_values.sh
+++ infra/gp-cluster-monitoring-config/helper/decrypt_alertmanager_config_from_values.sh
@@ -10,8 +10,8 @@ valuesFile="../../../../gepardec-run-cluster-configuration/cluster-applications/
 
 echo -e "\nACHTUNG: Du verwendest gerade: $(oc whoami --show-server=true)\nUnd folgendes values File: ${valuesFile}"
 
-for CERT in $(kubectl -n gp-infrastructure get secret --sort-by metadata.creationTimestamp | egrep "sealed-secret.*-key" | grep kubernetes | cut -d " " -f1); do
-  COMMAND=$(kubeseal --recovery-private-key <(oc -n gp-infrastructure get secret ${CERT} -o jsonpath="{.data.tls\.key}" | base64 -d) --recovery-unseal -f <(helm template -s templates/08-alertmanager-config-sealed-secret.yaml --set alertmanager.config=$(grep -A2 alertmanager.config ${valuesFile} | grep value | cut -d '"' -f2) ..) -o yaml | grep -o "alertmanager.yaml.*" | cut -d " " -f2 | base64 -d | tee alertmanager-from-values-${valuesFileSuffix}.yml)
+for CERT in $(kubectl get secret -n gp-infrastructure -l sealedsecrets.bitnami.com/sealed-secrets-key -o name); do
+  COMMAND=$(kubeseal --recovery-private-key <(oc -n gp-infrastructure get ${CERT} -o jsonpath="{.data.tls\.key}" | base64 -d) --recovery-unseal -f <(helm template -s templates/08-alertmanager-config-sealed-secret.yaml --set alertmanager.config=$(grep -A2 alertmanager.config ${valuesFile} | grep value | cut -d '"' -f2) ..) -o yaml | grep -o "alertmanager.yaml.*" | cut -d " " -f2 | base64 -d | tee alertmanager-from-values-${valuesFileSuffix}.yml)
   if [ "${COMMAND}" != "" ]; then
     echo "${COMMAND}"
     exit 0
--- infra/gp-cluster-update-checker/helpers/decrypt_slack_channel_config_from_values.sh
+++ infra/gp-cluster-update-checker/helpers/decrypt_slack_channel_config_from_values.sh
@@ -10,10 +10,8 @@ valuesFile="../../../../gepardec-run-cluster-configuration/cluster-applications/
 
 echo -e "\nACHTUNG: Du verwendest gerade: $(oc whoami --show-server=true)\nUnd folgendes values File: ${valuesFile}"
 
-for CERT in $(kubectl -n gp-infrastructure get secret --sort-by metadata.creationTimestamp | egrep "sealed-secret.*-key" | tac | grep kubernetes | cut -d " " -f1); do
-
-  COMMAND=$(kubeseal --recovery-private-key <(oc -n gp-infrastructure get secret ${CERT} -o jsonpath="{.data.tls\.key}" | base64 -d) --recovery-unseal -f <(helm template -s templates/01-sealed-secret.yaml --set slack.channel=$(grep -A2 slack.channel ${valuesFile} | grep value | cut -d '"' -f2) ..) -o yaml | grep -o "slackTargetChannel.*" | cut -d " " -f2 | base64 -d | tee slack-channel-from-values-${valuesFileSuffix}.yml)
-
+for CERT in $(kubectl get secret -n gp-infrastructure -l sealedsecrets.bitnami.com/sealed-secrets-key -o name); do
+  COMMAND=$(kubeseal --recovery-private-key <(oc -n gp-infrastructure get ${CERT} -o jsonpath="{.data.tls\.key}" | base64 -d) --recovery-unseal -f <(helm template -s templates/01-sealed-secret.yaml --set slack.channel=$(grep -A2 slack.channel ${valuesFile} | grep value | cut -d '"' -f2) ..) -o yaml | grep -o "slackTargetChannel.*" | cut -d " " -f2 | base64 -d | tee slack-channel-from-values-${valuesFileSuffix}.yml)
   if [ "$COMMAND" != "" ]; then
     echo "${COMMAND}"
     exit 0
