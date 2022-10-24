b025d2d0a3551e57a70e4d45c2900ca535b340b6
Herbert Wirnsberger <herbert.wirnsberger@gepardec.com>
2022-10-24 10:49:32 +0200

    GPX-471 - code review fix scripts, encrypt slackTargetChannel

--- infra/gp-cluster-monitoring-config/helper/decrypt_alertmanager_config_from_values.sh
+++ infra/gp-cluster-monitoring-config/helper/decrypt_alertmanager_config_from_values.sh
@@ -10,12 +10,10 @@ valuesFile="../../../../gepardec-run-cluster-configuration/cluster-applications/
 
 echo -e "\nACHTUNG: Du verwendest gerade: $(oc whoami --show-server=true)\nUnd folgendes values File: ${valuesFile}"
 
-CERTS=$(kubectl -n gp-infrastructure get secret --sort-by metadata.creationTimestamp | grep sealed-secrets-key | wc -l)
-
 for CERT in $(kubectl -n gp-infrastructure get secret --sort-by metadata.creationTimestamp | egrep "sealed-secret.*-key" | grep kubernetes | cut -d " " -f1); do
   COMMAND=$(kubeseal --recovery-private-key <(oc -n gp-infrastructure get secret ${CERT} -o jsonpath="{.data.tls\.key}" | base64 -d) --recovery-unseal -f <(helm template -s templates/08-alertmanager-config-sealed-secret.yaml --set alertmanager.config=$(grep -A2 alertmanager.config ${valuesFile} | grep value | cut -d '"' -f2) ..) -o yaml | grep -o "alertmanager.yaml.*" | cut -d " " -f2 | base64 -d | tee alertmanager-from-values-${valuesFileSuffix}.yml)
-
-  if COMMAND; then
+  if [ "${COMMAND}" != "" ]; then
+    echo "${COMMAND}"
     exit 0
   else
     echo '>>> next cert '
@@ -23,4 +21,3 @@ for CERT in $(kubectl -n gp-infrastructure get secret --sort-by metadata.creatio
 done
 
 echo "Kein Zertifikat hat funktioniert :c"
-
--- infra/gp-cluster-update-checker/helpers/generate_sealed_secret.sh
+++ infra/gp-cluster-update-checker/helpers/generate_sealed_secret.sh
@@ -9,5 +9,5 @@ then
 fi
 
 echo -e "Dein neues Sealed Secret, füge es ein in cluster-applications/gp-cluster-applications/values/values-*.yaml -> clusterUpdater.parameters.slack.channel.value:\n"
-oc create secret generic alertmanager-main --from-file=slack-channel.yml=${filename} --dry-run=client -o yaml -n openshift-monitoring | kubeseal -o yaml --cert <(kubeseal --fetch-cert --controller-name sealed-secrets-operator --controller-namespace gp-infrastructure) | grep slack-channel.yml | tr -s " " | cut -d " " -f3
+oc create secret generic cluster-update-secret --from-file=slackTargetChannel=${filename} --dry-run=client -o yaml -n gp-infrastructure | kubeseal -o yaml --cert <(kubeseal --fetch-cert --controller-name sealed-secrets-operator --controller-namespace gp-infrastructure) | grep slackTargetChannel | tr -s " " | cut -d " " -f3
 echo -e "\nACHTUNG: Du verwendest gerade: $(oc whoami --show-server=true)\n"
