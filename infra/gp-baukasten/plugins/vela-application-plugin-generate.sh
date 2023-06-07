0b80b6a67551abd286875eceb3d159069251589a
gattma <marko.gattringer@gepardec.com>
2023-06-07 15:26:10 +0200

    removed gp-baukasten chart

--- infra/gp-baukasten/plugins/vela-application-plugin-generate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-while IFS= read -r line || [[ -n "$line" ]]
-do
-  vela dry-run -f "$line"
-done < .baukasten
--- infra/gp-baukasten/plugins/vela-cue-transform-plugin-generate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-CUE_FILES=$(ls *.cue)
-for file in $CUE_FILES; do
-   vela def apply "$file" --dry-run -n {{ .Release.Namespace }};
-   echo "---"
-done;
